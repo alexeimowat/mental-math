b53284398a5ee841f95a5ec335e7131d304483f1
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2022-07-07 10:56:11 -0600

    created basic addition file with props and template. Just want to see if a number can be displayed

---
+++ mentalMath/src/components/math/addition.vue
@@ -0,0 +1,14 @@
+<script>
+defineProps ({
+    addend: {
+        num: Number,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>{{addend}} + {{addend}}</h1>
+    </div>
+</template>
