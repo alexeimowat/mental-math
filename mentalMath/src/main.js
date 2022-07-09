f4a75961ea9eb077549155bdbc012d7b41f2bcdb
alexeimowat <56664562+alexeimowat@users.noreply.github.com>
2022-07-09 10:31:15 -0600

    Added bootstrap to project

--- mentalMath/src/main.js
+++ mentalMath/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important here)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
+//createApp(App).use(BootstrapVue)
+//createApp(App).use(IconsPlugin)
 
 createApp(App).mount('#app')
